d6305c5cd73c71032d17933bcf046d7d182c9c19
Clement Nzau <clement.nzau@gmail.com>
2020-02-10 02:35:23 +0300

    Add basic Aad and delete animation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,9 @@
 import React, { useContext } from 'react';
+import {
+  CSSTransition,
+  Transition,
+  TransitionGroup
+} from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
@@ -13,13 +18,19 @@ const Contacts = () => {
 
   return (
     <>
-      {filtered !== null
-        ? filtered.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        : contacts.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map(contact => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </>
   );
 };
